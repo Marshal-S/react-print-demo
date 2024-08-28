52d8ec74e416a1acb06975a054b2d76a04e8b1ae
李帅 <lishuai@troila.com>
2024-08-28 16:10:16 +0800

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import ReactToPrint from "react-to-print";
+import { ComponentToPrint, PrintView } from "./print-view";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const componentRef = useRef(null);
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.tsx</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React
+                </a>
+                <ReactToPrint
+                    trigger={() => <a href="#">点此打印</a>}
+                    content={() => componentRef.current}
+                />
+                {/* <PrintView ref={componentRef} /> */}
+                <ComponentToPrint ref={componentRef} />
+
+                {/* <div ref={componentRef}>My cool content here!</div> */}
+            </header>
+        </div>
+    );
 }
 
 export default App;
